11ccb65eee008b3112b604ce883dc62e85e4ba97
frankdj412 <frankdj412@gmail.com>
2014-11-13 13:08:09 +0800

    Add: Convert all the cheat sheet of Emacs from org file to Dash

--- cheatsheets/Emacs.rb
+++ cheatsheets/Emacs.rb
@@ -4,35 +4,371 @@ cheatsheet do
   keyword 'emacs'
   source_url 'http://cheat.kapeli.com'
 
+  # Org mode
+  category do
+    id 'Org Mode'
+
+    # Outline Movement
+    entry do
+      name '**Outline Movement and cycling**'
+    end
+    entry do
+      command '-'
+      name 'Change bullet style'
+    end
+    entry do
+      command 'gh or ,u'
+      name 'Outline up heading'
+    end
+    entry do
+      command 'gj or ,n'
+      name 'Outline next heading'
+    end
+    entry do
+      command 'gk or ,p'
+      name 'Outline previous heading'
+    end
+
+    entry do
+      command '^ or H'
+      name 'To line begining'
+    end
+
+    entry do
+      command '$ or L'
+      name 'To line ending'
+    end
+
+    entry do
+      command ',t'
+      name 'Set tags'
+    end
+
+    entry do
+      command '<'
+      name 'Left indent'
+    end
+
+    entry do
+      command '>'
+      name 'Right indent'
+    end
+
+    #----------------------------------------
+
+    # Editing
+    entry do
+      name '**Editing**'
+    end
+
+    ## For link
+    entry do
+      name '*Link*'
+    end
+
+    entry do
+      command 'C-c C-l'
+      name 'Create a link'
+      notes 'org-insert-link'
+    end
+
+    entry do
+      command 'C-c C-o'
+      name 'Open the link'
+      notes 'org-open-at-point'
+    end
+
+    entry do
+      command 'C-c %'
+      name 'Mark internal link'
+    end
+
+    ## Table
+    entry do
+      name '*Table*'
+    end
+
+    entry do
+      command 'M-<left>/<right>'
+      name 'Move Column'
+    end
+
+    entry do
+      command 'M-<up>/<down>'
+      name 'Move Row'
+    end
+
+    entry do
+      command 'M-S-<left>/<right>'
+      name 'Delete current column'
+    end
+
+    entry do
+      command 'M-S-<up>/<down>'
+      name 'Delete current row'
+    end
+
+    entry do
+      command '- or C-c -'
+      name 'Insert ---'
+    end
+
+    entry do
+      command 'C-c <RET>'
+      name 'Insert --- and Jump to next line'
+    end
+
+    entry do
+      command 'C-c ^'
+      name 'Sort Table'
+    end
+
+    entry do
+      command 'Insert "<N>" at the cell'
+      name 'Specify the column width'
+    end
+
+    entry do
+      command 'C-c backquote'
+      name 'View the folded text'
+    end
+
+    entry do
+      command 'C-c C-c'
+      name 'Finish editing'
+    end
+
+    ## List
+    entry do
+      name '*List*'
+    end
+
+    entry do
+      command 'C-c C-c'
+      name '"[]" -> [x]'
+    end
+
+    ## Preview Latex Fragment
+    entry do
+      name '*Preview Latex Fragment*'
+    end
+
+    entry do
+      command 'C-u C-u C-c C-x C-l'
+      name 'Produce preview for all latex fragments'
+    end
+
+    entry do
+      command 'C-c C-x C-l'
+      name 'Produce preview of current subtree latex fragment'
+    end
+
+    entry do
+      command 'C-c C-c'
+      name 'Remove the overlay preview images'
+    end
+
+    entry do
+      command 'M-x cdlatex-command-help'
+      name '/To get a list of abbreviations of cdlatex/'
+    end
+
+    entry do
+      command 'C-c {'
+      name 'Insert Latex Template'
+    end
+
+    #----------------------------------------
+
+    # Basic Syntax
+    entry do
+      name '**Basic Syntax**'
+    end
+    entry do
+      command '*bold*'
+      name '**Bold** the text'
+    end
+    entry do
+      command '/italic/'
+      name 'Italic the text'
+    end
+    entry do
+      command '_underlined_'
+      name 'Underline the text'
+    end
+    entry do
+      command '=verbatim='
+      command '~code~'
+      name 'Monotone font'
+    end
+    entry do
+      command '+strike-through+'
+      name 'Delete line across the text'
+    end
+  end
+
+
+  # Magit
+  category do
+    id 'Magit'
+
+    # Adding Staging and Commit
+    entry do
+      name '**Adding Staging and Commit**'
+    end
+
+    ## Adding
+    entry do
+      name '*Add*'
+    end
+
+    entry do
+      command 's'
+      name 'Add untracked files to staging area'
+    end
+
+    entry do
+      command 'i'
+      name 'Add files -> .gitignore'
+    end
+
+    entry do
+      command 'C-u i'
+      name 'Prompt for -> .gitignore'
+    end
+
+    ## Stage Files
+    entry do
+      name '*Stage Files*'
+    end
+
+    entry do
+      command 's'
+      name 'Stage current hunk'
+    end
+
+    entry do
+      command 'S'
+      name 'Stage ALL hunks'
+    end
+
+    entry do
+      command 'u'
+      name 'Unstage current hunk'
+    end
+
+    entry do
+      command 'U'
+      name 'Unstage ALL hunks'
+    end
+
+    entry do
+      command '<tab>'
+      name 'See difference'
+    end
+
+    ## Commit
+    entry do
+      name '*Commit*'
+    end
+
+    entry do
+      command 'k'
+      name 'Discard uncommited changes'
+    end
+
+    entry do
+      command 'c'
+      name 'Prepare for commit'
+    end
+
+    entry do
+      command 'C-c C-c'
+      name 'Execute commit'
+    end
+
+    entry do
+      command 'C-c C-a'
+      name 'Make next commit an amend'
+    end
+
+
+    #----------------------------------------
+
+    # Branching
+    entry do
+      name '**Branching**'
+    end
+
+    entry do
+      command 'b'
+      name 'Switch to another branch'
+    end
+
+    entry do
+      command 'B'
+      name 'Create and switch another branch'
+    end
+
+    #----------------------------------------
+
+    # Pushing and Pulling
+    entry do
+      name '**Pushing and Pulling**'
+    end
+
+    entry do
+      command 'p'
+      name 'git push'
+    end
+
+    entry do
+      command 'C-u p'
+      name 'Push to specified repository'
+    end
+
+    entry do
+      command 'f'
+      name 'git remote update'
+    end
+
+    entry do
+      command 'F'
+      name 'git pull'
+    end
+
+  end
+
+  # Below are the Default Cheat Sheet
   category do
     id 'Motion'
 
-    entry do 
+    entry do
       command 'C-a'
       notes '`beginning-of-line`'
       name 'Move to the beginning of line'
     end
-    entry do 
+    entry do
       command 'C-e'
       notes '`end-of-line`'
       name 'Move to the end of line'
     end
-    entry do 
+    entry do
       command 'C-f'
       notes '`forward-char`'
       name 'Move forward a character'
     end
-    entry do 
+    entry do
       command 'C-b'
       notes '`backward-char`'
       name 'Move back a character'
     end
-    entry do 
+    entry do
       command 'M-f'
       notes '`forward-word`'
       name 'Move forward a word'
     end
-    entry do 
+    entry do
       command 'M-b'
       notes '`backward-word`'
       name 'Move backward a word'
@@ -96,47 +432,47 @@ cheatsheet do
   category do
     id 'Killing and Yanking'
 
-    entry do 
+    entry do
       command 'C-k'
       notes '`kill-line`'
       name 'Kill the text from point to the end of the line'
     end
-    entry do 
+    entry do
       command 'C-x Rubout '
       notes '`backward-kill-line`'
       name 'Kill backward to the beginning of the line'
     end
-    entry do 
+    entry do
       command 'C-u'
       notes '`unix-line-discard`'
       name 'Kill backward from point to the beginning of the line'
     end
-    entry do 
+    entry do
       command 'M-d'
       notes '`kill-word`'
       name 'Kill from point to the end of the current word'
     end
-    entry do 
+    entry do
       command 'M-Rubout'
       notes '`backward-kill-word`'
       name 'Kill the word behind point'
     end
-    entry do 
+    entry do
       command 'C-w'
       notes '`unix-word-rubout`'
       name 'Kill the word behind point, using white space as a word boundary'
     end
-    entry do 
+    entry do
       command 'M-\ '
       notes '`delete-horizontal-space`'
       name 'Delete all spaces and tabs around point'
     end
-    entry do 
+    entry do
       command 'C-y'
       notes '`yank`'
       name 'Yank the top of the kill ring into the buffer at point'
     end
-    entry do 
+    entry do
       command 'M-y'
       notes '`yank-pop`'
       name 'Rotate the kill ring, and yank the new top'
@@ -146,17 +482,17 @@ cheatsheet do
   category do
     id 'Keyboard Macros'
 
-    entry do 
+    entry do
       command 'C-x ('
       notes '`start-kbd-macro`'
       name 'Begin saving the chars typed into the current keyboard macro'
     end
-    entry do 
+    entry do
       command 'C-x )'
       notes '`end-kbd-macro`'
       name 'End saving the chars typed into the current keyboard macro'
     end
-    entry do 
+    entry do
       command 'C-x e'
       notes '`call-last-kbd-macro`'
       name 'Re-execute the last keyboard macro defined'
@@ -166,17 +502,17 @@ cheatsheet do
   category do
     id 'Completing'
 
-    entry do 
+    entry do
       command 'TAB'
       notes '`complete`'
       name 'Attempt to perform completion on the text before point'
     end
-    entry do 
+    entry do
       command 'M-?'
       notes '`possible-completions`'
       name 'List the possible completions of the text before point. '
     end
-    entry do 
+    entry do
       command 'M-*'
       notes '`insert-completions`'
       name 'Insert all completions of the text before point generated by possible-completions'
@@ -238,7 +574,7 @@ cheatsheet do
       name 'Insert the n-th argument to the previous command at point'
     end
     entry do
-      command 'M-' 
+      command 'M-'
       command 'M-_'
       notes '`yank-last-arg`'
       name 'Insert the last argument to the  previous command'
@@ -261,7 +597,7 @@ cheatsheet do
       name 'Abort the current editing command and ring the terminals bell'
     end
     entry do
-      command 'M-a' 
+      command 'M-a'
       command 'M-b'
       notes '`do-uppercase-version`'
       name 'If the metafield char x is lowercase run the command that is bound to uppercase char'
@@ -336,4 +672,3 @@ cheatsheet do
     * Converted by [@erggo](https://github.com/erggo).
     END
 end
-
